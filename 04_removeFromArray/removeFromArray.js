7513d432ebaa98841a3a59879db9ece1f2837320
DimitrijeB-96 <mitabackalic96@gmail.com>
2023-01-19 19:16:15 +0100

    Task completed, passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...num) {
+        let x = num.length;
+        for( let j = 0; j < x; j++) {
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i] !== num[j]) {
+                    continue;
+                } else {
+                    arr.splice(i, 1);
+                }
+            }
+        }
+        return arr;
 };
 
 // Do not edit below this line
