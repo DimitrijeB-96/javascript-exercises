feb31e8cfedfedb0d307fbe5da6b4f20c5a12976
DimitrijeB-96 <mitabackalic96@gmail.com>
2023-01-19 21:19:54 +0100

    Task completed, passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    let sum = 0;
+    
+    if ( first > second) {
+        first += second;
+        second = first - second;
+        first -= second; 
+    } 
+    if (first < 0 || second < 0 || !Number.isInteger(first) || !Number.isInteger(second)) {
+        sum = 'ERROR';
+        return sum;
+    }     
+    else {
+        for(let i = first; i <= second; i++) {
+            sum += i;
+        }
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
