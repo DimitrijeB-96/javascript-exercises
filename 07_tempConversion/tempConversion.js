546e9d4fa5c92104a77eb798495f5e001d76e21d
DimitrijeB-96 <mitabackalic96@gmail.com>
2023-01-20 11:11:04 +0100

    Task completed, passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let convert = ((x - 32) * (5/9));
+  let temperature = Math.round(convert * 10) / 10;
+  return temperature;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  let convert = ((x * (9/5)) + 32)
+  let temperature = Math.round(convert * 10) / 10;
+  return temperature;
 };
 
 // Do not edit below this line
